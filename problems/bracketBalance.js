1e85d6c47e9714c6198b89ee19af7d390e1449cd
steve <stevenharlan@gmail.com>
2016-11-26 01:00:02 -0800

    balancedParens

--- problems/bracketBalance.js
+++ problems/bracketBalance.js
@@ -1,1 +1,49 @@
-console.log('okays')
+/*
+ * write a function that takes a string of text and returns true if
+ * the parentheses are balanced and false otherwise.
+ *
+ * Example:
+ *   balancedParens('(');  // false
+ *   balancedParens('()'); // true
+ *   balancedParens(')(');  // false
+ *   balancedParens('(())');  // true
+ *
+ * Step 2:
+ *   make your solution work for all types of brackets
+ *
+ * Example:
+ *  balancedParens('[](){}'); // true
+ *  balancedParens('[({})]');   // true
+ *  balancedParens('[(]{)}'); // false
+ *
+ * Step 3:
+ * ignore non-bracket characters
+ * balancedParens(' var wow  = { yo: thisIsAwesome() }'); // true
+ * balancedParens(' var hubble = function() { telescopes.awesome();'); // false
+ *
+ *
+ */
+
+function balancedParens(str) {
+  const openerStack = [];
+  const pairs = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+  };
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str.charAt(i) === '(' || str.charAt(i) === '{' || str.charAt(i) === '[') {
+      openerStack.push(str.charAt(i));
+    } else if (str.charAt(i) === ')' || str.charAt(i) === '}' || str.charAt(i) === ']') {
+      if (openerStack[openerStack.length - 1] === pairs[str.charAt(i)]) {
+        openerStack.pop();
+      }
+    }
+  }
+
+  if (openerStack.length > 0) return false;
+  return true;
+}
+
+export default balancedParens;
--- problems/test/bracketbalance.test.js
+++ problems/test/bracketbalance.test.js
@@ -1,1 +1,45 @@
 const expect = require('chai').expect
+
+import balancedParens from '../bracketBalance'
+
+describe('bracket balance function', () => {
+  it('should be a function', () =>{
+    expect(balancedParens).to.be.a('function')
+  });
+  it('should return false with "("', () => {
+    const result = balancedParens('(');
+    expect(result).to.be.false;
+  });
+  it('should return true with "()"', () => {
+    const result = balancedParens('()');
+    expect(result).to.be.true;
+  });
+  it('should return false with ")("', () => {
+    const result = balancedParens(')(');
+    expect(result).to.be.false;
+  });
+  it('should return true with "(())"', () => {
+    const result = balancedParens('(())');
+    expect(result).to.be.true;
+  });
+  it('should return true with "[](){}"', () => {
+    const result = balancedParens('[](){}');
+    expect(result).to.be.true;
+  });
+  it('should return true with "[({})]"', () => {
+    const result = balancedParens('[({})]');
+    expect(result).to.be.true;
+  });
+  it('should return false with "[(]{)}"', () => {
+    const result = balancedParens('[(]{)}');
+    expect(result).to.be.false;
+  });
+  it('should return true with "var wow  = { yo: thisIsAwesome() }"', () => {
+    const result = balancedParens('var wow  = { yo: thisIsAwesome() }');
+    expect(result).to.be.true;
+  });
+  it('should return false with "var hubble = function() { telescopes.awesome();"', () => {
+    const result = balancedParens('var hubble = function() { telescopes.awesome();');
+    expect(result).to.be.false;
+  });
+});
